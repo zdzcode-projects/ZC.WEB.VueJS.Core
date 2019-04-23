2a9eeaf83d855be91ff9200ce5efd6b697910276
Zdzislaw Kochanski <contato@zdzcode.com.br>
2019-04-23 20:22:10 -0300

    ajuste teste

--- src/lib.js
+++ src/lib.js
@@ -1,31 +1,31 @@
 import ZCDataTable from './components/ZCDataTable'
 
 
-import zcbirthdaydate from './components/zcbirthdaydate.vue'
-import zccep from './components/zccep.vue'
-import zccheckbox from './components/zccheckbox.vue'
-import zccnpj from './components/zccnpj.vue'
-import zccolor from './components/zccolor.vue'
-import zccombobox from './components/zccombobox.vue'
-import zccomboboxserver from './components/zccomboboxserver.vue'
-import zccpf from './components/zccpf.vue'
-import zccpfcnpj from './components/zccpfcnpj.vue'
-import zcdate from './components/zcdate.vue'
-import zcdatetime from './components/zcdatetime.vue'
-import zcemail from './components/zcemail.vue'
-import zcemailconfirmation from './components/zcemailconfirmation.vue'
-import zcfile from './components/zcfile.vue'
-import zcmoney from './components/zcmoney.vue'
-import zcnumber from './components/zcnumber.vue'
-import zcpassword from './components/zcpassword.vue'
-import zcpasswordconfirmation from './components/zcpasswordconfirmation.vue'
-import zcphone from './components/zcphone.vue'
-import zcselect from './components/zcselect.vue'
-import zcslider from './components/zcslider.vue'
-import zcstring from './components/zcstring.vue'
-import zcswitch from './components/zcswitch.vue'
-import zctextarea from './components/zctextarea.vue'
-import zctime from './components/zctime.vue'
+import zcbirthdaydate from './components/ZCBirthdaydate.vue'
+import zccep from './components/ZCCep.vue.vue'
+// import zccheckbox from './components/zccheckbox.vue'
+// import zccnpj from './components/zccnpj.vue'
+// import zccolor from './components/zccolor.vue'
+// import zccombobox from './components/zccombobox.vue'
+// import zccomboboxserver from './components/zccomboboxserver.vue'
+// import zccpf from './components/zccpf.vue'
+// import zccpfcnpj from './components/zccpfcnpj.vue'
+// import zcdate from './components/zcdate.vue'
+// import zcdatetime from './components/zcdatetime.vue'
+// import zcemail from './components/zcemail.vue'
+// import zcemailconfirmation from './components/zcemailconfirmation.vue'
+// import zcfile from './components/zcfile.vue'
+// import zcmoney from './components/zcmoney.vue'
+// import zcnumber from './components/zcnumber.vue'
+// import zcpassword from './components/zcpassword.vue'
+// import zcpasswordconfirmation from './components/zcpasswordconfirmation.vue'
+// import zcphone from './components/zcphone.vue'
+// import zcselect from './components/zcselect.vue'
+// import zcslider from './components/zcslider.vue'
+// import zcstring from './components/ZCString.vuezcstring.vue'
+// import zcswitch from './components/zcswitch.vue'
+// import zctextarea from './components/zctextarea.vue'
+// import zctime from './components/zctime.vue'
 
 const components = {
   ZCDataTable,
@@ -33,29 +33,29 @@ const components = {
 
   zcbirthdaydate,
   zccep,
-  zccheckbox,
-  zccnpj,
-  zccolor,
-  zccombobox,
-  zccomboboxserver,
-  zccpf,
-  zccpfcnpj,
-  zcdate,
-  zcdatetime,
-  zcemail,
-  zcemailconfirmation,
-  zcfile,
-  zcmoney,
-  zcnumber,
-  zcpassword,
-  zcpasswordconfirmation,
-  zcphone,
-  zcselect,
-  zcslider,
-  zcstring,
-  zcswitch,
-  zctextarea,
-  zctime,
+  //   zccheckbox,
+  //   zccnpj,
+  //   zccolor,
+  //   zccombobox,
+  //   zccomboboxserver,
+  //   zccpf,
+  //   zccpfcnpj,
+  //   zcdate,
+  //   zcdatetime,
+  //   zcemail,
+  //   zcemailconfirmation,
+  //   zcfile,
+  //   zcmoney,
+  //   zcnumber,
+  //   zcpassword,
+  //   zcpasswordconfirmation,
+  //   zcphone,
+  //   zcselect,
+  //   zcslider,
+  //   zcstring,
+  //   zcswitch,
+  //   zctextarea,
+  //   zctime,
 }
 
 
