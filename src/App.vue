748b5cbe3628225af96a1540025fd88aa36fc464
Zdzislaw Kochanski <contato@zdzcode.com.br>
2019-04-22 22:53:09 -0300

    Funcionou

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img src="./assets/logo.png">
+    <hello></hello>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Hello from './components/hello'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Hello
   }
 }
 </script>
---
+++ src/lib.js
@@ -0,0 +1,5 @@
+import zcWebVuejsCore from './components/hello'
+import './styles/lib.scss'
+import './styles/lib.styl'
+
+export default zcWebVuejsCore
