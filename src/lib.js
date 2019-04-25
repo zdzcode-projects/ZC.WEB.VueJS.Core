e1c59b840a2c6f4e7eb3a9071411db4e7620b00e
Zdzislaw Kochanski <contato@zdzcode.com.br>
2019-04-25 18:19:56 -0300

    teste passando config do veevalidate

--- src/lib.js
+++ src/lib.js
@@ -127,22 +127,19 @@ const config = {
 export default {
   // The install method will be called with the Vue constructor as         
   // the first argument, along with possible options
-  install(Vue, options) {
+  install(Vue, config) {
     // Add or modify global methods or properties.
     //Vue.yourMethod = (value) => value
     // Add a component or directive to your plugin, so it will be installed globally to your project.
 
 
-    // if (!!config && config.hasOwnProperty(veeValidateConfig))
-    // {
+    if (!!config && config.hasOwnProperty('veeValidateConfig')) {
+      Vue.use(VeeValidate, config.veeValidateConfig);
+    } else {
 
-    // }
-    // else
-    // {
+    }
 
-    // }
 
-    //Vue.use(VeeValidate, config);
 
     Object.entries(components).forEach(([name, component]) => {
       Vue.component(name, component)
